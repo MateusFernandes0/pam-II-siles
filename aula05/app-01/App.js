ef71b6c50f57fd267083ec12791b62d60147792b
Fulano de Tal <fulanodetal@exemplo.br>
2024-04-04 08:30:54 -0300

    anfoijsngiodsg

---
+++ aula05/app-01/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>choro de mormaço ...... enfia o aço</Text>  <Image style={styles.logo} source={require('aula05\app-01\assets\mormas.png')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
